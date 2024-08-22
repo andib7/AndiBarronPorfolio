87cdfb35fcf44286d228d58f9bb316d5d0b51e7f
Andi Barron <andibarron7@gmail.com>
2024-08-21 21:30:50 -0700

    Added Profile Picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import profilePic from '../assets/me.jpg'; // Adjust path as necessary
 
 function Home({ onButtonClick }) {
   return (
     <section id="home" className="cover-page">
       <div className="container">
-        <img src="path/to/profile-pic.jpg" alt="Profile" className="profile-pic" />
+        <img src={profilePic} alt="Profile" className="profile-pic" />
         <h1>Andi Barron</h1>
         <h2>Full-Stack Developer</h2>
         <p>Welcome to my portfolio!</p>
